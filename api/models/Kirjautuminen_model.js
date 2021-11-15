9e6e8d928767d5dc0ab333b6626b5f988dcafc04
EliVeliS <t0seel00@students.oamk.fi>
2021-11-15 23:49:55 +0200

    alustus

--- api/models/Kirjautuminen_model.js
+++
@@ -1,9 +0,0 @@
-const db = require('../database');
-
-const login={
-  checkPassword: function(username, callback) {
-      return db.query('SELECT password FROM user_table WHERE username = ?',[username], callback); 
-    }
-};
-          
-module.exports = login;
