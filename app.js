b2afe5f9ef0e955538e79d10c98f5ff208b4ef49
EliVeliS <t0seel00@students.oamk.fi>
2021-11-15 20:03:16 +0200

    test

--- app.js
+++ app.js
@@ -2,31 +2,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
-
-app.use(helmet());
-app.use(cors());
-const dotenv = require('dotenv');
-dotenv.config();
-const basicAuth = require('express-basic-auth');
-app.use(basicAuth( { authorizer: myAuthorizer, authorizeAsync:true, } ))
-
-function myAuthorizer(username, password, cb){
-    if(username===process.env.authUser && password ===process.env.authPass){
-        return cb(null, true);
-    }
-    else{
-        return cb(null, false);
-    }
-}
-
-var bookRouter = require('./routes/book');
-var borrowerRouter = require('./routes/borrower');
-var userRouter = require('./routes/user');
-var loginRouter = require('./routes/login');
-var bankRouter = require('./routes/bank');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,10 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/book', bookRouter);
-app.use('/borrower', borrowerRouter);
-app.use('/user', userRouter);
-app.use('/login', loginRouter);
-app.use('/bank', bankRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
