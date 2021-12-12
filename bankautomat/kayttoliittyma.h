e3dee711ffdcf2f1c5b49e0bddcc4b044b55e760
ihju <t0ihju00@students.oamk.fi>
2021-12-12 18:20:59 +0200

    koko roska toimii jollakin tapaa

--- bankautomat/kayttoliittyma.h
+++ bankautomat/kayttoliittyma.h
@@ -6,7 +6,7 @@
 #include <QNetworkAccessManager>
 #include <QJsonDocument>
 #include <QString>
-<<<<<<< HEAD
+
 #include <QTimer>
 #include "nostarahaa.h"
 #include "muuttujat.h"
@@ -14,12 +14,12 @@
 
 #include "muuttujat.h"
 #include "lahjoita.h"
-=======
+
 #include <QObject>
 
 #include "muuttujat.h"
 #include "nayta_saldo.h"
->>>>>>> fa370cb1e7dfd94e994c4f1e168217213af4e109
+
 
 namespace Ui {
 class kayttoliittyma;
